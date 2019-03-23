feb6a027453323ee396ee605a90d142f3912581d
asas12 <john13john@interia.pl>
2019-03-23 22:36:08 +0100

    Saving working lexer.

--- lab1/scanner.py
+++ lab1/scanner.py
@@ -27,33 +27,15 @@ tokens = (
           'MULASSIGN',
           'DIVASSIGN',
 
-          'ADD',
-          'SUB',
-          'MUL',
-          'DIV',
 
           'LESS_EQUAL',
           'GREATER_EQUAL',
           'NOT_EQUAL',
           'EQUAL',
-          'ASSIGN',
-          'LESS',
-          'GREATER',
-
-          'LPAREN',
-          'RPAREN',
-          'LPAREN_SQUARE',
-          'RPAREN_SQUARE',
-          'LPAREN_CURLY',
-          'RPAREN_CURLY',
 
           'FLOATNUM',
           'INTNUM',
 
-          'SLICE',
-          'TRANSPOSE',
-          'COMMA',
-          'COLON',
 
           'STRING',
           'ID') + tuple(reserved.values())
@@ -74,38 +56,27 @@ t_SUBASSIGN = r'-='
 t_MULASSIGN = r'\*='
 t_DIVASSIGN = r'/='
 
-t_ADD    = r'\+'
-t_SUB   = r'-'
-t_MUL   = r'\*'
-t_DIV  = r'/'
 
 t_LESS_EQUAL = r'<='
 t_GREATER_EQUAL = r'>='
 t_NOT_EQUAL = '!='
 t_EQUAL = '=='
-t_ASSIGN = '='
-t_LESS = '<'
-t_GREATER = '>'
 
-t_LPAREN  = r'\('
-t_RPAREN  = r'\)'
-t_LPAREN_SQUARE = r'\['
-t_RPAREN_SQUARE = r']'
-t_LPAREN_CURLY = r'{'
-t_RPAREN_CURLY = r'}'
-
-t_SLICE = r':'
-t_TRANSPOSE = r'\''
-t_COMMA = r','
-t_COLON = r';'
 
 #t_STRING = r'"[^"]*"'
-t_STRING = r'"([^"](\\")?)*"'
+#t_STRING = r'"([^"]?(\\")?)*"'
+def t_STRING(t):
+    r'"([^\\"]+|\\.)*"'
+    t.value = t.value[1:-1]
+    #t.value = re.sub()
+    return t
+
 
 def t_FLOATNUM(t):
-    r'([0-9]+\.[0-9]*|[0-9]*\.[0-9]+)([Ee][+-]?[0-9]*)?'
+    r'([0-9]+\.[0-9]*|\.[0-9]+)([Ee][+-]?[0-9]*)?'
     t.value = float(t.value)
     return t
+
 
 def t_INTNUM(t):
     r'\d+'
@@ -119,7 +90,7 @@ def t_ID(t):
     return t
 
 
-literals = "+-*/()"
+literals = r"+-*/(){}[]:<>=,;'"
 
 t_ignore = '  \t'
 
@@ -137,5 +108,6 @@ def t_error(t) :
 def find_column(input, token):
     line_start = input.rfind('\n', 0, token.lexpos) + 1
     return (token.lexpos - line_start) + 1
+
 
 lexer = lex.lex()
